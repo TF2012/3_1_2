d41f269e3aee5226aae2ec160ce42c5d5908b9db
blackshinemusic <144614162+blackshinemusic@users.noreply.github.com>
2023-12-19 21:11:35 +0300

    .

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,10 +12,11 @@ import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder bCryptPasswordEncoder;
@@ -79,5 +84,15 @@ public class UserServiceImpl implements UserService {
     public void removeUser(long id) {
         Optional<User> user = userRepository.findById(id);
         user.ifPresent(userRepository::delete);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("The user with this username not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.getRoles().stream().map(
+                        role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toList()));
     }
 }
